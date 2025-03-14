e00e69291dcfc54db8e3bb857ba9a86f8f5c4d3d
CihanAkkus <cihann.akkus2@gmail.com>
2025-03-14 14:44:30 +0300

    commit

---
+++ FirebaseInstaClone/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  FirebaseInstaClone
+//
+//  Created by Cihan Akkuş on 13.03.2025.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
