fa21c4ceda3363436bc7207dd23fb26ca3c7c3c9
CihanAkkus <cihann.akkus2@gmail.com>
2025-03-15 13:07:32 +0300

    commit

--- FirebaseInstaClone/FeedCell.swift
+++ FirebaseInstaClone/FeedCell.swift
@@ -9,13 +9,6 @@ import UIKit
 
 class FeedCell: UITableViewCell {
     
-    @IBOutlet weak var userEmailLabel: UILabel!
-    
-    @IBOutlet weak var commentLabel: UILabel!
-    
-    @IBOutlet weak var userImageView: UIImageView!
-    
-    @IBOutlet weak var likeLabel: UILabel!
     
     
     
@@ -23,14 +16,12 @@ class FeedCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
+    
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
-    }
-    
-    @IBAction func likeButton(_ sender: Any) {
     }
     
     
--- FirebaseInstaClone/FeedViewController.swift
+++ FirebaseInstaClone/FeedViewController.swift
@@ -5,12 +5,9 @@ import UIKit
 
 
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     
-    
-    
-    @IBOutlet weak var tableView: UITableView!
-    
+    let tableView = UITableView()
     
     
     
@@ -20,11 +17,31 @@ class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.frame = view.bounds
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        view.addSubview(tableView)
+        
         
         
 
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        /*
+        cell.userEmailLabel.text = "test@test.com"
+        cell.likeLabel.text = "10"
+        cell.commentLabel.text = "comment"
+        cell.userImageView.image = UIImage(named: "selectimage")
+         */
+        return cell
+    }
     
     
     
